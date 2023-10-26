d7b0be6800b69954fd2b809dc8459bc0c250fc8c
Josenei Santana <joseneisatana@yahoo.com>
2023-10-25 22:06:55 -0300

    finalizando tarefa

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+    
+    $('#telefone').mask('(00) 00000-0000');
+    
+   
+    $('#cep').mask('00000-000');
+    
+});
+
+
+
